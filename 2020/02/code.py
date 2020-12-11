4a7ced86348f312d40bc16c5f574f50cf8e24b74
Giorgos Keramidas <gkeramidas@gmail.com>
2020-12-10 21:06:31 -0800

    Advent of Code 2020 - day 02 part 02

--- 2020/02/code.py
+++ 2020/02/code.py
@@ -20,9 +20,38 @@ def check(t):
     return (cmin <= count and count <= cmax)
 
 
+def check2(t):
+    m = re.match(r"^(\d+)-(\d+)\s+(\S): (.+)$", t)
+    if not m:
+        # Cannot invalid policy which does not parse.
+        return False
+
+    pa = int(m.group(1))
+    pb = int(m.group(2))
+    char = m.group(3)[0]
+    password = m.group(4)
+
+    # Positions pa and pb when converted to zero-based index *must* fall
+    # within the bounds of len(password).
+    pa -= 1
+    pb -= 1
+    if (
+        pa < 0 or pa >= len(password) or
+        pb < 0 or pb >= len(password)
+    ):
+        return False
+
+    pair = [password[pa], password[pb]]
+    count = len([c for c in pair if c == char])
+    return count == 1
+
+
 def main():
     lines = sys.stdin.readlines()
     valid = [check(t.rstrip('\n')) for t in lines]
+    count = len([v for v in valid if v])
+    print("{} valid lines".format(count))
+    valid = [check2(t.rstrip('\n')) for t in lines]
     count = len([v for v in valid if v])
     print("{} valid lines".format(count))
 
